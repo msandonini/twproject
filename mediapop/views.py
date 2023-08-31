eeb4e0e39d2c88fa310d9186101bf34244a0a499
Mirco Sandonini <mirco.ms16@gmail.com>
2023-08-31 22:46:19 +0200

    Basic media page logic

--- mediapop/views.py
+++ mediapop/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from mediapop.models import Media
+
 
 # Create your views here.
 
@@ -13,3 +15,15 @@ class IndexView(TemplateView):
 
 class MediaView(TemplateView):
     template_name = "mediapop/media.html"
+
+    def get(self, request, *args, **kwargs):
+        media = Media.objects.values()
+
+        print("[MEDIA] ", media)
+
+        context = {}
+
+        if len(media) > 0:
+            context["media"] = media
+
+        return render(request=request, template_name=self.template_name, context=context)
