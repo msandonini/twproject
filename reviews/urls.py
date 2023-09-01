fd21107be8f9bc778ae2b84d4331eb322e3c90a8
Mirco Sandonini <mirco.ms16@gmail.com>
2023-09-01 19:30:56 +0200

    Update

--- reviews/urls.py
+++ reviews/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path
 
+from reviews import views
+
+app_name = "reviews"
 urlpatterns = [
+    re_path("^$|^/$|^index/$", views.IndexView.as_view(), name="index")
 ]
