bb1c0d4a69007514594fe357d51cca178eb32508
Mirco Sandonini <mirco.ms16@gmail.com>
2023-09-09 21:56:52 +0200

    Created forum models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from mediapop.models import Media
+
+
 # Create your models here.
+
+
+class ForumThread(models.Model):
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Forum thread '{self.title}' about {self.media.name}"
+
+
+class ThreadComment(models.Model):
+    thread = models.ForeignKey(ForumThread, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by @{self.user.username} in thread '{self.thread.title}'"
